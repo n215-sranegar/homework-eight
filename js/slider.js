99f507d419d65f6fdac7853f598dab3180e76952
Stephanie Ranegar <stephanie.ranegar@gmail.com>
2022-04-01 10:16:07 -0400

    About page almost finished. All code cloned from Career Planning portfolio piece and adding pages using JQuery

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+//slick slider logic
+$('.slider').slick({
+    adaptiveHeight: true,
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    slidesToScroll: 1,
+    arrows: false,
+    dots: true,
+    pauseOnDotsHover: true
+});
